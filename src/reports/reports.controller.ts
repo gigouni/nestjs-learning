0e2bbe50253cb5450d3eae078a044d5c15c9cc66
Nicolas GIGOU <nicolas.gigou@s3pweb.com>
2022-07-27 08:46:20 +0200

    feat(reports): implement the reports creation

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guards';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  async createReport(@Body() body: CreateReportDto) {
+    console.log(body);
+    return this.reportsService.create(body);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  // @InjectRepository(Report) is necessary because we'reusing a generic Report type with the Repository one
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(reportDto: CreateReportDto): Promise<Report> {
+    // The `create(...) method creates the Report entity instance but doesn't persist it
+    // It allows a prevalidation of the data based on decorators within the `report.entity.ts` file
+    const createdReport = this.repo.create(reportDto);
+    return this.repo.save(createdReport);
+  }
+}
